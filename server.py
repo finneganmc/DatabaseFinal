d80d64f6ff8825a7847d7a30ce79e5e1d05d7efe
finneganmc <130228140+finneganmc@users.noreply.github.com>
2023-11-21 11:21:17 -0500

    reorg

--- server.py
+++ server.py
@@ -250,8 +250,46 @@ def register():
   return render_template('login.html')
   
 @app.route('/tweet', methods=['POST'])
+def tweet(): 
+  tt  = request.form['text']
+  tl = "English"
+  pt = str(datetime.now())
+  with open("./data.json",'r',encoding='utf-8') as load_f:
+    d = json.load(load_f)
+  tid = d["Tweetcount"]+1
+  d["Tweetcount"] += 1
+  uid = d[CurUser]
+  Changedata("Tweetcount",d["Tweetcount"]+1)
+  params_dict = {"tid":tid,"tt":tt,"tl":tl,"pt":pt, "uid":uid}
+  g.conn.execute(text("INSERT INTO Tweets(tweet_id,tweet_text,tweet_language,post_time) VALUES (:tid,:tt,:tl,:pt)"), params_dict)
+  g.conn.execute(text("INSERT INTO from_user(tweet_id,user_id) VALUES (:tid, :uid)"), params_dict)
+  g.conn.commit()
+  return render_template('index')
+
 @app.route('/like', methods=['POST'])
+def like():
+  si = str(datetime.now())
+  with open("./data.json",'r',encoding='utf-8') as load_f:
+    d = json.load(load_f)
+  tid = request.form['id']
+  uid = d[CurUser]
+  Changedata("Tweetcount",d["Tweetcount"]+1)
+  params_dict = {"tid": tid, "uid":uid, "si":si}
+  g.conn.execute(text("INSERT INTO Likes(tweet_id, user_id,since) VALUES (:tid,:uid,:si)"), params_dict)
+  g.conn.commit()
+  return render_template('index')
+
 @app.route('/follow', methods=['POST'])
+def follow():
+  si = str(datetime.now())
+  with open("./data.json",'r',encoding='utf-8') as load_f:
+    d = json.load(load_f)
+  uid = d[CurUser]
+  fud = request.form["id"]
+  params_dict = {"uid":uid, "fud":fud, "si":si}
+  g.conn.execute(text("INSERT INTO Likes(tweet_id, user_id,since) VALUES (:tid,:uid,:si)"), params_dict)
+  g.conn.commit()
+  return render_template('index')
 
 # @app.route('/login')
 # def login():
